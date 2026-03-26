7ab5d9b12b8ed425aaa012a13da7042f5f331b44
muzammil <muzammil.myshopygram@gmail.com>
2026-03-26 21:53:31 +0530

    second push

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello everyone! this is my first java program iam making continuously learning java and i am loving it  and i am sure that i will be a good java developer in future            ");
     }
 }
